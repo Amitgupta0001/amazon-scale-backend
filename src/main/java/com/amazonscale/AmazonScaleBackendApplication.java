fdb692ae5fdd152f6dd0278a7572e842830d6b5f
Amitgupta0001 <Amitgupta001503@gmail.com>
2026-07-21 17:16:49 +0530

    build: initialize Spring Boot project

---
+++ src/main/java/com/amazonscale/AmazonScaleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.amazonscale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonScaleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonScaleBackendApplication.class, args);
+	}
+
+}
